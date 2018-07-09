4edde8e1b8032a1cb67ee75ba75f212fa52d2425
Idan Yadgar <idanyadgar@gmail.com>
2018-07-10 02:37:42 +0300

    Main screen done

---
+++ index.js
@@ -0,0 +1,40 @@
+var screens = {
+    main: 'main',
+    mainTyping: 'main-typing',
+    mainListening: 'main-listening',
+    tripOptions: 'trip-options',
+    selectedTrip: 'selected-trip',
+    liveTracking: 'live-tracking',
+    thankYou: 'thank-you'
+};
+
+function goToScreen(screen) {
+    document.querySelectorAll('.screen').forEach(function(e) {
+        e.style.display = 'none';
+    });
+    
+    document.querySelector('#' + screen + '-screen').style.display = 'block';
+}
+
+document.querySelectorAll('.search-box').forEach(function(e) {
+    e.addEventListener('click', function(e) {
+        goToScreen(screens.mainTyping);
+    });
+});
+
+document.querySelectorAll('.microphone').forEach(function(e) {
+    e.addEventListener('click', function(e) {
+        e.stopPropagation();
+
+        goToScreen(screens.mainListening);
+        setTimeout(function() {
+            goToScreen(screens.mainTyping);
+        }, 3000);
+    });
+});
+
+document.querySelectorAll('#favorites > .box').forEach(function(e) {
+    e.addEventListener('click', function(e) {
+        goToScreen(screens.tripOptions);
+    });
+});
